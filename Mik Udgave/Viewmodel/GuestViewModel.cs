fdd70b75f6052eade7d3cea8996beea3a56b18cf
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-04-06 12:36:51 +0200

    *GuestViewModel +PropertyChangedEventHandler +Selected field - mangler at ændre class samt Navn

--- Mik Udgave/Viewmodel/GuestViewModel.cs
+++ Mik Udgave/Viewmodel/GuestViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mik_Udgave.Viewmodel
 {
-    class GuestViewModel
+    class GuestViewModel : INotifyPropertyChanged
     {
+
+
+        #region vores PropertyChangedEventHandler 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Select event prop & instance field
+
+        private CLASSNAVN selectedÆNDRE;
+        public CLASSNAVN SelectedÆNDRE
+        {
+            get { return selectedÆNDRE; }
+            set
+            {
+                selectedEvent = value;
+                OnPropertyChanged(nameof(SelectedÆNDRE));
+            }
+        }
+
+        #endregion
     }
 }
